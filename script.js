cda5912d605acdea2ac59b3f72480296dd970cc9
Abdul Amir Alioghli <104595848+Amiralioghli@users.noreply.github.com>
2025-07-05 19:53:03 -0400

    Initial Update

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".switch");
+let bulbTop = document.querySelector(".bulb-top");
+let bulbBottom = document.querySelector(".bulb-bottom");
+
+let isOpen = false;
+
+btn.addEventListener("click", function () {
+  if (isOpen) {
+    btn.classList.remove("on");
+    bulbBottom.classList.remove("bulbOn");
+    bulbTop.classList.remove("bulbOn");
+  } else {
+    btn.classList.add("on");
+    bulbTop.classList.add("bulbOn");
+    bulbBottom.classList.add("bulbOn");
+  }
+  isOpen = !isOpen;
+});
